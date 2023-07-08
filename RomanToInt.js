cc0cf19a13df71d5371b6781abaa633c862bf271
paresiqbal <thecrowdel@gmail.com>
2023-07-08 23:00:43 +0700

    add roman to integer

---
+++ RomanToInt.js
@@ -0,0 +1,26 @@
+s = "XV";
+
+let romantToInt = function (s) {
+  symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  value = 0;
+
+  for (let i = 0; i < s.length - 1; i += 1) {
+    symbols[s[i]] < symbols[s[i + 1]]
+      ? (value -= symbols[s[i]])
+      : (value += symbols[s[i]]);
+  }
+
+  return value + symbols[s[s.length - 1]];
+};
+
+let result = romantToInt(s);
+console.log(result);
